293557b84c9fd97c765b99242e79cff76acca635
Yused23 <134747476+Yused23@users.noreply.github.com>
2024-09-18 12:53:12 -0500

    Se sube el proyecto
    Se suben los archivos necesarios.

---
+++ java/com/mycompany/calcularedad/CalcularEdad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcularedad;
+
+/**
+ *
+ * @author michel.sanchez
+ */
+public class CalcularEdad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
